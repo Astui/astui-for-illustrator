bd1a905cfcd3fa97c5caec1e653a8ab4ec50a70f
Scott Lewis <scott@iconify.it>
2018-09-13 21:27:51 -0400

    Build extension delete older builds

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cp -R client build/client
 cp -R csxs build/csxs
 cp -R host build/host
 
-./bin/ZXPSignCmd -sign build builds/com.astute.astui-illustrator.$VERS.zxp ./bin/selfDB.p12 Alias2Mocha7 -tsa https://www.safestamper.com/tsa
+./bin/ZXPSignCmd -sign build builds/com.astui.ill.$VERS.zxp ./bin/selfDB.p12 Alias2Mocha7 -tsa https://www.safestamper.com/tsa
 
 if [ -d build ]; then
     rm -Rf build
