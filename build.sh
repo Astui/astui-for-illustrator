e048e76cf543a954c11277e64d10c214a30fa813
Scott Lewis <scott@iconify.it>
2018-09-25 16:02:48 -0700

    Comment build.sh script and rename builds directory to dist

--- build.sh
+++ build.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
+# Create version string.
+
 VERS=`date +%s`
+
+# If the `build` directory exists, delete it.
 
 if [ -d build ]; then
     rm -Rf build
 fi
 
+# Create a clean build directory.
+
 mkdir build
+
+# Copy source code to build directory.
+
 cp -R client build/client
 cp -R csxs build/csxs
 cp -R host build/host
 
-rm builds/*
+# Delete old builds.
 
-./bin/ZXPSignCmd -sign build builds/com.astui.ill.$VERS.zxp ./bin/selfDB.p12 Alias2Mocha7 -tsa https://www.safestamper.com/tsa
+rm dist/*
+
+# Build and sign the extension.
+
+./bin/ZXPSignCmd -sign build dist/com.astui.ill.$VERS.zxp ./bin/selfDB.p12 Alias2Mocha7 -tsa https://www.safestamper.com/tsa
+
+# Delete the build directory.
 
 if [ -d build ]; then
     rm -Rf build
